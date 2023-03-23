483b1796dccaf69708ac3e685f64bafef52efaf5
Sebastien Morazzani <mrgy.sebastien@gmail.com>
2023-03-24 00:27:04 +0100

    AJout des filtres au clics

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,13 +1,35 @@
 const gallery = document.querySelector(".gallery");
+const filtersContainer = document.querySelector(".filters-container");
+const filters = new Set();
 
-
-
-
+// Fetch Works
 const fetchGet = async () => {
-  await fetch("http://localhost:5678/api/works").then((res) =>
-    res.json().then((data) => (works = data))
-  );
-  console.log(works[0]);
+  // lien avec l'API
+  await fetch("http://localhost:5678/api/works")
+    .then((res) => res.json().then((data) => (works = data)))
+    .catch((err) => {
+      console.log(`Erreur : ${err}`);
+    });
+  console.log(works[2]);
+
+  // Ajout des travaux
+  galleryWork(works);
+};
+
+// Fetch Catégories
+const fetchCategory = async () => {
+  await fetch("http://localhost:5678/api/categories")
+    .then((res) => res.json().then((cat) => (categories = cat)))
+    .catch((err) => {
+      console.log(`Erreur : ${err}`);
+    });
+
+  //Ajout des filtres
+  filtres(categories);
+};
+
+// Implémente les Images dans la gallery
+function galleryWork(works) {
   works.map((work) => {
     const post = document.createElement("figure");
     post.innerHTML = `
@@ -16,6 +38,47 @@ const fetchGet = async () => {
     `;
     gallery.appendChild(post);
   });
-};
+}
+
+// Créer les filtres
+function filtres(categories) {
+  categories.map((filter) => {
+    filters.add(filter.name);
+  });
+
+  // Tranforme l'objet set en array
+  const filtersArray = Array.from(filters);
+  //Créer les éléments buttons
+  for (let i = 0; i < filtersArray.length; i++) {
+    const filtre = document.createElement("button");
+    filtre.classList.add(
+      "filter-btn",
+      `${filtersArray[i].split(" ").join("")}`
+    );
+    filtre.innerText = filtersArray[i];
+    filtersContainer.appendChild(filtre);
+  }
+
+  //Filtre au clic
+  const btnTous = document.getElementById("tous");
+  btnTous.addEventListener("click", () => {
+    gallery.innerHTML = "";
+    fetchGet();
+  });
+
+  const buttons = document.querySelectorAll(".filter-btn");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      buttons.forEach((button) => button.classList.remove("active"));
+      gallery.innerHTML = "";
+      const workFiltered = works.filter((work) => {
+        return work.category.name === e.target.innerText;
+      });
+      button.classList.add("active");
+      galleryWork(workFiltered);
+    });
+  });
+}
 
 fetchGet();
+fetchCategory();
