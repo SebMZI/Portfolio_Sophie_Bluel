6c6ef3c33bf7cf3ea0b1af3afcaa67333a6490b1
Sebastien Morazzani <mrgy.sebastien@gmail.com>
2023-03-24 12:47:13 +0100

    Intégration de la login page

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,6 @@
+const loginForm = document.getElementById("login-form");
+const emailInput = document.getElementById("email");
+const mdpInput = document.getElementById("mdp");
+const errorMsg = document.querySelector(".mdp-oublie");
+
+
