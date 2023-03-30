a70129c9544cf93afdc5c4ef64db11b93c5b097a
Sebastien Morazzani <mrgy.sebastien@gmail.com>
2023-03-30 17:21:19 +0200

    Ajout de la fonctionnalité DELETE, pour supprimer les différents travaux

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -8,7 +8,13 @@ const modalGallery = document.querySelector(".modal-gallery-work");
 const header = document.querySelector("header");
 const portfolio = document.getElementById("portfolio");
 const log = document.querySelector(".log-link-title");
+const modal1 = document.querySelector(".pictures-gallery");
+const modal2 = document.querySelector(".add-picture-gallery");
+const addPictureBtn = document.querySelector(".addPicture-btn");
+const returnArrow = document.querySelector(".return-arrow");
 const filters = new Set();
+let tokenValue = localStorage.token;
+console.log(tokenValue);
 
 console.log(log);
 // Fetch Works
@@ -38,10 +44,27 @@ const fetchCategory = async () => {
   filtres(categories);
 };
 
+const fetchDelete = async (id) => {
+  await fetch("http://localhost:5678/api/works/" + id, {
+    method: "DELETE",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${tokenValue}`,
+    },
+    mode: "cors",
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.log("Il y a eu une erreur sur le Fetch: " + err));
+};
 // Implémente les Images dans la gallery
 function galleryWork(works) {
   works.map((work) => {
     const post = document.createElement("figure");
+    post.setAttribute("id", `${work.id}`);
     post.innerHTML = `
     <img src=${work.imageUrl} alt="image de ${work.title}">
     <figcaption>${work.title}</figcaption> 
@@ -50,17 +73,21 @@ function galleryWork(works) {
   });
 }
 
+// Ajout de la gallery dans la modale
 function workGallery(works) {
   works.map((work) => {
     const workPost = document.createElement("figure");
+    workPost.setAttribute("id", `${work.id}`);
     workPost.innerHTML = `
     <div class="workgallery-container">
-      <i class="fa-solid fa-trash-can trash-icon"></i>
+      <i id="${work.id}"  class="fa-solid fa-trash-can trash-icon" ></i>
       <img class="modal-image" src=${work.imageUrl} alt="image de ${work.title}">
     </div>    
     <figcaption>éditer</figcaption> 
     `;
     modalGallery.appendChild(workPost);
+
+    deleteImage(workPost);
   });
 }
 
@@ -84,12 +111,14 @@ function filtres(categories) {
   }
 
   //Filtre au clic
+  //Filtre pour le bouton Tous
   const btnTous = document.getElementById("tous");
   btnTous.addEventListener("click", () => {
     gallery.innerHTML = "";
     fetchGet();
   });
 
+  // Filtre pour les catégories suivantes
   const buttons = document.querySelectorAll(".filter-btn");
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
@@ -127,13 +156,37 @@ log.addEventListener("click", () => {
   log.innerText = "login";
 });
 
+// Affiche la modale
 function toggleModal() {
   modalContainer.classList.toggle("target");
 }
-
 modalTriggers.forEach((trigger) =>
   trigger.addEventListener("click", toggleModal)
 );
+
+function showAddPictureModal() {
+  modal1.style.display = "none";
+  modal2.style.display = "flex";
+}
+
+addPictureBtn.addEventListener("click", showAddPictureModal);
+
+function returnModal1() {
+  modal1.style.display = "block";
+  modal2.style.display = "none";
+}
+
+returnArrow.addEventListener("click", returnModal1);
+
+function deleteImage(imgValue) {
+  const deleteIcon = document.querySelectorAll(".trash-icon");
+  deleteIcon.forEach((delIcon) => {
+    delIcon.addEventListener("click", (e) => {
+      fetchDelete(parseInt(e.target.id));
+      console.log(typeof parseInt(e.target.id));
+    });
+  });
+}
 
 //Appel des différentes fonctions
 fetchGet();
